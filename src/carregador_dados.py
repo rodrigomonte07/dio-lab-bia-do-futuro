ca1aa1a68eab4a0b7dafccfc42d44733bad34ade
RODRIGO MONTE COSTA <rodrigomontecosta@gmail.com>
2026-06-12 13:53:41 -0300

    Update carregador_dados.py

--- src/carregador_dados.py
+++ src/carregador_dados.py
@@ -1,16 +1,36 @@
-import pandas as pd
 import json
+from pathlib import Path
+
+import pandas as pd
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / "data"
+
 
 def carregar_dados():
-    
-    transacoes = pd.read_csv("../data/transacoes.csv")
 
-    historico = pd.read_csv("../data/historico_atendimento.csv")
+    transacoes = pd.read_csv(DATA_DIR / "transacoes.csv")
 
-    with open("../data/perfil_investidor.json", encoding="utf-8") as arquivo:
+    historico = pd.read_csv(DATA_DIR / "historico_atendimento.csv")
+
+    with open(
+        DATA_DIR / "perfil_investidor.json",
+        encoding="utf-8"
+    ) as arquivo:
+
         perfil = json.load(arquivo)
 
-    with open("../data/produtos_financeiros.json", encoding="utf-8") as arquivo:
+    with open(
+        DATA_DIR / "produtos_financeiros.json",
+        encoding="utf-8"
+    ) as arquivo:
+
         produtos = json.load(arquivo)
 
-    return transacoes, historico, perfil, produtos
+    return (
+        transacoes,
+        historico,
+        perfil,
+        produtos
+    )
